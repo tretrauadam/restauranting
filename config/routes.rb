9b846e314c8a1c693a59a8f4f59d3cd5f90b87c0
adamzermane <adamzermane92@yahoo.com>
2022-01-09 18:16:22 +0100

    seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+# collection
+#member
+
+
+  resources :restaurants do
+    collection do
+      get :top
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
